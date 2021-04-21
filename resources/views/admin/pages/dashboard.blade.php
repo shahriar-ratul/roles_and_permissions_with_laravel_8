782e8fda67c917ca25e9eca499b5e86334455949
Shahriar Ratul <ratul794@gmail.com>
2021-04-21 20:03:24 +0600

    admin Dashboard add

---
+++ resources/views/admin/pages/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.app')
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+
+
+    </div>
+    <!-- /.row -->
+    </div><!-- /.container-fluid -->
+
+@endsection
