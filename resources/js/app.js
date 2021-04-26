6f3e901afbe0abe73ac9be482ae9febcc490b19b
Shahriar Ratul <ratul794@gmail.com>
2021-04-27 01:21:15 +0600

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,46 +9,46 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
-// import VueToastr from "vue-toastr";
-// Vue.use(VueToastr, {
-//     defaultTimeout: 3000,
-//     defaultPosition: "toast-top-right",
-//     defaultProgressBar: false,
-//     defaultProgressBarValue: 0,
-// });
+import VueToastr from "vue-toastr";
+Vue.use(VueToastr, {
+    defaultTimeout: 3000,
+    defaultPosition: "toast-top-right",
+    defaultProgressBar: false,
+    defaultProgressBarValue: 0,
+});
 
-// import moment from 'moment';
+import moment from 'moment';
 
-// Vue.filter("date", function(created){
-//     return moment(created).format('MMMM Do YYYY, h:mm:ss a');
-// });
+Vue.filter("date", function(created){
+    return moment(created).format('MMMM Do YYYY, h:mm:ss a');
+});
 
 
 
-// import Swal from 'sweetalert2'
-// window.swal = Swal;
-// const Toast = Swal.mixin({
-//     toast: true,
-//     position: 'top-end',
-//     showConfirmButton: false,
-//     timer: 3000,
-//     timerProgressBar: true,
-//     didOpen: (toast) => {
-//       toast.addEventListener('mouseenter', Swal.stopTimer)
-//       toast.addEventListener('mouseleave', Swal.resumeTimer)
-//     }
-// });
+import Swal from 'sweetalert2'
+window.swal = Swal;
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
 
-// window.toast = Toast;
+window.toast = Toast;
 
-// import { Form, HasError, AlertError } from 'vform'
+import { Form, HasError, AlertError } from 'vform'
 
-// window.Form = Form;
+window.Form = Form;
 
-// Vue.component(HasError.name, HasError)
-// Vue.component(AlertError.name, AlertError)
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
 
-// require('./component');
+require('./component');
 
 const app = new Vue({
     el: '#app'
